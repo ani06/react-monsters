0e9750ca106101714b2ad1d2fd3c14f52a46ae8b
Anirudh Mohan <m.anirudh1992@gmail.com>
2019-09-30 04:11:07 +0530

    initial create react app with class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters : [
+        {
+          name: 'Frankenstein'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
+    }
+  }
+  render(){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>{this.state.string}</p>
+      <button onClick={() => this.setState({string: 'Hello Anirudh'})}>Change Text</button>
+    </header>
+  </div>
+    );
+  }
 }
 
 export default App;
