e455854ae267a62cbe767be8d8ddf08177be8003
Anirudh Mohan <m.anirudh1992@gmail.com>
2019-09-30 21:38:44 +0530

    Updates

---
+++ precache-manifest.1779781f9138e3eceb43b6888e8b2ecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ca2f3c8f8256d63fafb3a92f7b7dda",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "c331603aa1c14cabefbd",
+    "url": "/react-monsters/static/css/main.e5baec62.chunk.css"
+  },
+  {
+    "revision": "8ae996a654f27ebbabb8",
+    "url": "/react-monsters/static/js/2.2c8a6238.chunk.js"
+  },
+  {
+    "revision": "c331603aa1c14cabefbd",
+    "url": "/react-monsters/static/js/main.0f492a41.chunk.js"
+  },
+  {
+    "revision": "ca96d79ea22a083e77dd",
+    "url": "/react-monsters/static/js/runtime~main.9e6231db.js"
+  }
+]);
